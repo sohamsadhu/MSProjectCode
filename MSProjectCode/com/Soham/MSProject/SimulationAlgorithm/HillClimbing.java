b9d936042e33d1426d427f1a63024c9693f2104b
Soham Sadhu <sxs9174@rit.edu>
2014-04-17 20:24:01 -0400

    Just put my thoughts on experiment structure in Hill Climbing part for the moment.

--- MSProjectCode/com/Soham/MSProject/SimulationAlgorithm/HillClimbing.java
+++ MSProjectCode/com/Soham/MSProject/SimulationAlgorithm/HillClimbing.java
@@ -1,5 +1,14 @@
 package com.Soham.MSProject.SimulationAlgorithm;
 
+// Design of to be experiment.
+// First hierarchy would be the folder with chaining value,
+// The folder numbers would be 32, 64, 128, 256, 512, 1024.
+// Next in each of these folders would 4 attack algorithms. So each folder 
+// would have hill climb, simulated annealing, taboo search, random selection.
+// Each of these algorithm will have 4 digest sizes consisting of 224, 256, 384, 512.
+// Each of the digest size will have 3 SHA-3 finalist algorithm folder.
+// Each of the finalist algorithm folder will have 3 rounds folder to them.
+// Each round folder will have 20 text files.
 public class HillClimbing 
 {
 }
