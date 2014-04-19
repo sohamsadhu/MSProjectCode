84fbe7c7ce8df1b70703dab7823df9f9017b6850
Soham Sadhu <sxs9174@rit.edu>
2014-04-18 22:56:45 -0400

    Add a new file for experiment.

---
+++ MSProjectCode/com/Soham/MSProject/SimulationAlgorithm/Experiment.java
@@ -0,0 +1,10 @@
+package com.Soham.MSProject.SimulationAlgorithm;
+
+public class Experiment 
+{
+  public void startExperiment( String cv, String m_collision, String diglen, String sha3,
+      String rounds, String flipend )
+  {
+    
+  }
+}
