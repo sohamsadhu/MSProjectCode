bccd0556fad17fe5ea938d6741ca61db313e23cb
Soham Sadhu <sxs9174@rit.edu>
2014-05-18 23:03:06 -0400

    Update the file to collect collision and average number data.

--- MSProjectCode/com/Soham/MSProject/Input/CreateOutputXls.java
+++ MSProjectCode/com/Soham/MSProject/Input/CreateOutputXls.java
@@ -43,14 +43,53 @@ public class CreateOutputXls
     return (sum/20);
   }
   
+  public int[] getCollisionTrials( String path_begin, String chain_value, String find_collision, 
+      String hash_method, String digest_length, String round, String flip_folder )
+  {
+    String file_path = new String("./"+ path_begin +"/"+ chain_value +"/"+ find_collision 
+        +"/"+ digest_length +"/"+ hash_method +"/"+ round +"/"+ flip_folder +"/");
+    String[] files = new String[]{"1.txt", "2.txt", "3.txt", "4.txt", "5.txt", "6.txt", "7.txt",
+        "8.txt", "9.txt", "10.txt", "11.txt", "12.txt", "13.txt", "14.txt", "15.txt", "16.txt",
+        "17.txt", "18.txt", "19.txt", "20.txt"};
+    int collision = 0;
+    int trials    = 0;
+    for( String file_name : files )
+    {
+      File file = new File(file_path + file_name);
+      try
+      {
+        Scanner scan = new Scanner( file );
+        int j = 0;
+        while( scan.hasNextLine() && j < 1 )
+        {
+          int temp = Integer.parseInt( scan.nextLine().split(":")[1] );
+          if( temp > 0 )
+          {
+            collision++;
+            if( temp > trials ) {
+              trials = temp;
+            }
+          }
+          scan.nextLine();
+          j++;
+        }
+        scan.close();
+      }
+      catch( IOException ioex ) {
+        ioex.printStackTrace();
+      }
+    }
+    return (new int[]{collision, trials});
+  }
+  
   public void fileLoop()
   {
     String   path_begin = "Output/";
-    String[] cv         = new String[]{"32"}; //"32", "64", "128", "256", "512"
+    String[] cv         = new String[]{"32_128trials"}; //"32", "64", "128", "256", "512"
     String[] fc         = new String[]{"HillClimbing"};
     String[] sha3       = new String[]{"BLAKE", "Groestl", "Keccak"};    //
-    String[] digest_len = new String[]{"512"};
-    String[] rounds     = new String[]{"3", "4"};
+    String[] digest_len = new String[]{"224", "256", "384", "512"};
+    String[] rounds     = new String[]{"1", "2", "3"};
     String[] flipend    = new String[]{"Start", "Middle", "End"};
     // Ugly bow shaped loop, coming up!
     for( String chain_value : cv ) {
@@ -59,9 +98,10 @@ public class CreateOutputXls
           for( String digest_length : digest_len ) {
             for( String round : rounds ) {
               for( String flip_folder : flipend ) {
-                float avg = getAverage( path_begin, chain_value, find_collision, hash_method, 
-                    digest_length, round, flip_folder );
-                System.out.println("avg "+ hash_method +" "+ round +" "+ flip_folder +" "+ avg);
+                int [] collision = getCollisionTrials( path_begin, chain_value, find_collision, 
+                    hash_method, digest_length, round, flip_folder );
+                System.out.println(hash_method +" "+ digest_length +" "+ round +" "+ flip_folder 
+                    +" collision "+ collision[0] +" attempts "+ collision[1]);
               }
             }
           }
